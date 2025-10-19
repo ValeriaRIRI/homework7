b877551114410b521f129af1d6f3d1d041031cf1
Валерия Пономарева <ponomarevavaleria86@gmail.com>
2025-10-19 22:27:55 +0300

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int c;
+	printf("Введите порядковый номер дня: ");
+	scanf("%d", &c);
+
+	switch (c) {
+		case 1:
+			printf("Понедельник");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		case 2:
+			printf("Вторник");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		case 3:
+			printf("Среда");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		case 4:
+			printf("Четверг");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		case 5:
+			printf("Пятница");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		case 6:
+			printf("Суббота");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		case 7:
+			printf("Воскресенье");
+			printf("\nОсталось дней: %d", 7 - c);
+			break;
+		default:
+			printf("День недели не определен");
+	}
+	return 0;
+}
